6510434093418c841211a37aca5b32f4bac6537e
Sabrina <sabrina.fiester@gmail.com>
2015-03-10 13:42:55 -0500

    Contact Test

--- public/mail/contact_me.php
+++ public/mail/contact_me.php
@@ -1,4 +1,8 @@
 <?php
+composer require wildbit/postmark-php
+use Postmark\PostmarkClient;
+
+$client = new PostmarkClient("4fdc7a2a-22d7-480d-ad89-b30be1400e31");
 // Check for empty fields
 if(empty($_POST['name'])  		||
    empty($_POST['email']) 		||
@@ -21,6 +25,6 @@ $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
 $headers = "From: noreply@sabrina.io\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";	
-mail($to,$email_subject,$email_body,$headers);
+$sendResult = $client->sendEmail("noreply@sabrina.io",$to,$email_subject,$email_body);
 return true;			
 ?>
